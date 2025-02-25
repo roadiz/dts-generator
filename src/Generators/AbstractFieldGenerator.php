fcc01a8769f94e3f5cf042e6de7bf5af3f3c0adc
roadiz-ci <ambroise+ci@roadiz.io>
2025-02-25 20:04:26 +0000

    chore: Bumped

--- src/Generators/AbstractFieldGenerator.php
+++ src/Generators/AbstractFieldGenerator.php
@@ -12,10 +12,19 @@ abstract class AbstractFieldGenerator
     // Four spaces
     public const INDENTATION_MARK = '    ';
 
+    protected NodeTypeFieldInterface $field;
+    protected ParameterBag $nodeTypesBag;
+
+    /**
+     * @param NodeTypeFieldInterface $field
+     * @param ParameterBag $nodeTypesBag
+     */
     public function __construct(
-        protected readonly NodeTypeFieldInterface $field,
-        protected readonly ParameterBag $nodeTypesBag,
+        NodeTypeFieldInterface $field,
+        ParameterBag $nodeTypesBag
     ) {
+        $this->field = $field;
+        $this->nodeTypesBag = $nodeTypesBag;
     }
 
     protected function getNullableAssertion(): string
@@ -27,18 +36,18 @@ abstract class AbstractFieldGenerator
 
     private function getDeclaration(): string
     {
-        return static::INDENTATION_MARK.
-            $this->field->getVarName().
-            $this->getNullableAssertion().': '.
+        return static::INDENTATION_MARK .
+            $this->field->getVarName() .
+            $this->getNullableAssertion() . ': ' .
             $this->getType();
     }
 
     public function getContents(): string
     {
         return implode(PHP_EOL, [
-            $this->getIntroduction(),
-            $this->getDeclaration(),
-        ]);
+                $this->getIntroduction(),
+                $this->getDeclaration()
+            ]);
     }
 
     protected function getIntroductionLines(): array
@@ -51,16 +60,19 @@ abstract class AbstractFieldGenerator
         }
 
         if (!empty($this->field->getGroupName())) {
-            $lines[] = 'Group: '.$this->field->getGroupName();
+            $lines[] = 'Group: ' . $this->field->getGroupName();
         }
 
         return $lines;
     }
 
+    /**
+     * @return string
+     */
     public function getIntroduction(): string
     {
         return implode(PHP_EOL, array_map(function (string $line) {
-            return static::INDENTATION_MARK.'// '.$line;
+            return static::INDENTATION_MARK . '// ' . $line;
         }, $this->getIntroductionLines()));
     }
 }
