669d3b0698f8dddd3ef91895cc9e7e512cc71b89
roadiz-ci <ambroise+ci@roadiz.io>
2025-10-23 13:23:11 +0000

    chore: bumped

--- src/Generators/EnumFieldGenerator.php
+++ src/Generators/EnumFieldGenerator.php
@@ -10,15 +10,20 @@ final class EnumFieldGenerator extends AbstractFieldGenerator
     {
         switch (true) {
             case $this->field->isEnum():
-                $defaultValues = $this->field->getDefaultValuesAsArray();
-                if (!empty($defaultValues) && count($defaultValues) > 0) {
-                    $defaultValues = array_map(function (string $value) {
-                        return '\''.$value.'\'';
-                    }, $defaultValues);
-
-                    return implode(' | ', $defaultValues).' | null';
+                if (!empty($this->field->getDefaultValues())) {
+                    $defaultValues = array_filter(
+                        array_map(
+                            'trim',
+                            explode(',', $this->field->getDefaultValues())
+                        )
+                    );
+                    if (count($defaultValues) > 0) {
+                        $defaultValues = array_map(function (string $value) {
+                            return '\'' . $value . '\'';
+                        }, $defaultValues);
+                        return implode(' | ', $defaultValues) . ' | null';
+                    }
                 }
-
                 return 'string';
             case $this->field->isMultiple():
                 return 'Array<string>';
@@ -30,10 +35,9 @@ final class EnumFieldGenerator extends AbstractFieldGenerator
     protected function getIntroductionLines(): array
     {
         $lines = parent::getIntroductionLines();
-        if (!empty($this->field->getDefaultValuesAsArray())) {
-            $lines[] = 'Possible values: '.json_encode($this->field->getDefaultValuesAsArray());
+        if (!empty($this->field->getDefaultValues())) {
+            $lines[] = 'Possible values: ' . $this->field->getDefaultValues();
         }
-
         return $lines;
     }
 }
