9299f6a228310a729f1a6a431bd6bbe9c44cb882
roadiz-ci <ambroise+ci@roadiz.io>
2026-04-29 08:08:20 +0000

    Merge branch hotfix/v2.7.19

--- src/DeclarationGeneratorFactory.php
+++ src/DeclarationGeneratorFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Typescript\Declaration;
 
+use RZ\Roadiz\Contracts\NodeType\NodeTypeClassLocatorInterface;
 use RZ\Roadiz\Contracts\NodeType\NodeTypeFieldInterface;
 use RZ\Roadiz\Contracts\NodeType\NodeTypeInterface;
 use RZ\Roadiz\Typescript\Declaration\Generators\AbstractFieldGenerator;
@@ -15,15 +16,14 @@ use RZ\Roadiz\Typescript\Declaration\Generators\NodeTypeGenerator;
 use RZ\Roadiz\Typescript\Declaration\Generators\ScalarFieldGenerator;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
-final class DeclarationGeneratorFactory
+final readonly class DeclarationGeneratorFactory
 {
-    public function __construct(private readonly ParameterBag $nodeTypesBag)
-    {
+    public function __construct(
+        private ParameterBag $nodeTypesBag,
+        private NodeTypeClassLocatorInterface $nodeTypeClassLocator,
+    ) {
     }
 
-    /**
-     * @return ParameterBag
-     */
     public function getNodeTypesBag(): ParameterBag
     {
         return $this->nodeTypesBag;
@@ -34,29 +34,20 @@ final class DeclarationGeneratorFactory
         return $bool ? 'true' : 'false';
     }
 
-    /**
-     * @param NodeTypeInterface $nodeType
-     *
-     * @return NodeTypeGenerator
-     */
     public function createForNodeType(NodeTypeInterface $nodeType): NodeTypeGenerator
     {
         return new NodeTypeGenerator(
             $nodeType,
-            $this
+            $this,
+            $this->nodeTypeClassLocator
         );
     }
 
-    /**
-     * @param NodeTypeFieldInterface $field
-     *
-     * @return AbstractFieldGenerator
-     */
     public function createForNodeTypeField(NodeTypeFieldInterface $field): AbstractFieldGenerator
     {
         return match (true) {
             $field->isDocuments() => new DocumentsFieldGenerator($field, $this->nodeTypesBag),
-            $field->isNodes() => new NodeReferencesFieldGenerator($field, $this->nodeTypesBag),
+            $field->isNodes() => new NodeReferencesFieldGenerator($this->nodeTypeClassLocator, $field, $this->nodeTypesBag),
             $field->isChildrenNodes() => new ChildrenNodeFieldGenerator($field, $this->nodeTypesBag),
             $field->isMultiple(), $field->isEnum() => new EnumFieldGenerator($field, $this->nodeTypesBag),
             default => new ScalarFieldGenerator($field, $this->nodeTypesBag),
