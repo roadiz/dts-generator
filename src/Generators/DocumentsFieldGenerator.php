501ddd940079f1e64de9f574b6ac6cdb3b66d074
roadiz-ci <ambroise+ci@roadiz.io>
2026-05-26 15:44:19 +0000

    chore: bumped

--- src/Generators/DocumentsFieldGenerator.php
+++ src/Generators/DocumentsFieldGenerator.php
@@ -6,13 +6,11 @@ namespace RZ\Roadiz\Typescript\Declaration\Generators;
 
 final class DocumentsFieldGenerator extends AbstractFieldGenerator
 {
-    #[\Override]
     protected function getNullableAssertion(): string
     {
         return ''; // always available even if empty
     }
 
-    #[\Override]
     protected function getType(): string
     {
         return 'Array<RoadizDocument>';
