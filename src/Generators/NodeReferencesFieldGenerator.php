547b35e68d6b292bf793c7b7e072f1ecff336ea2
roadiz-ci <ambroise+ci@roadiz.io>
2026-05-26 15:36:39 +0000

    chore: bumped

--- src/Generators/NodeReferencesFieldGenerator.php
+++ src/Generators/NodeReferencesFieldGenerator.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\Typescript\Declaration\Generators;
 
-use RZ\Roadiz\Contracts\NodeType\NodeTypeClassLocatorInterface;
-use RZ\Roadiz\Contracts\NodeType\NodeTypeFieldInterface;
 use RZ\Roadiz\Contracts\NodeType\NodeTypeInterface;
-use Symfony\Component\HttpFoundation\ParameterBag;
 
 final class NodeReferencesFieldGenerator extends AbstractFieldGenerator
 {
-    public function __construct(
-        private readonly NodeTypeClassLocatorInterface $nodeTypeClassLocator,
-        NodeTypeFieldInterface $field,
-        ParameterBag $nodeTypesBag,
-    ) {
-        parent::__construct($field, $nodeTypesBag);
-    }
-
     #[\Override]
     protected function getNullableAssertion(): string
     {
@@ -57,7 +46,7 @@ final class NodeReferencesFieldGenerator extends AbstractFieldGenerator
             return 'RoadizNodesSources';
         }
 
-        return implode(' | ', array_map($this->nodeTypeClassLocator->getSourceEntityClassName(...), $nodeTypes));
+        return implode(' | ', array_map(fn (NodeTypeInterface $nodeType) => $nodeType->getSourceEntityClassName(), $nodeTypes));
     }
 
     #[\Override]
